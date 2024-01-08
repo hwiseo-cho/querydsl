4a793ccfec708cf03ccab1295defd16cbcff08ba
hwiseo <gnltj37@naver.com>
2024-01-08 21:00:09 +0900

    jpa가 제공하는 querydsl 기능 2 ~ 3

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -2,12 +2,14 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
@@ -22,9 +24,19 @@ import static study.querydsl.entity.QTeam.team;
 
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
+
+    /*extends QuerydslRepositorySupport*/
+    /**
+     * Creates a new {@link QuerydslRepositorySupport} instance for the given domain type.
+     *
+     * @param domainClass must not be {@literal null}.
+     */
+    /*public MemberRepositoryCustomImpl(Class<?> domainClass) {
+        super(domainClass);
+    }*/
+
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
-
     public MemberRepositoryCustomImpl(EntityManager em) {
         this.em = em;
         this.queryFactory = new JPAQueryFactory(em);
@@ -32,6 +44,24 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     @Override
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        // QuerydslRepositorySupport 으로 작성
+        /*from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .fetch();*/
+
         return queryFactory
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
@@ -78,6 +108,38 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
         return new PageImpl<>(content, pageable, total);
     }
+
+    // QuerydslRepositorySupport 으로 작성
+    // sort 불가
+    /* @Override
+    public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
+
+        PQLQuery<MemberTeamDto> query = from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ));
+
+        JPQLQuery<MemberTeamDto> jpqlQuery = getQuerydsl().applyPagination(pageable, query);
+
+        jpqlQuery.fetch();
+
+        List<MemberTeamDto> content = result.getResults();
+        long total = result.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+        return null;
+    }*/
 
     @Override
     public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
